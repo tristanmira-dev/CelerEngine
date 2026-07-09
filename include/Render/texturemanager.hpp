556a2e8897df3748fca4a3537cbe382b40089546
Tristan Mira <tristanmira.dev@gmail.com>
2026-07-09 15:22:43 +0800

    texture manager

---
+++ include/Render/texturemanager.hpp
@@ -0,0 +1,17 @@
+#ifndef RENDER_TEXTUREMANAGER_HPP
+#define RENDER_TEXTUREMANAGER_HPP
+
+#include "image.hpp"
+#include ""
+
+class TextureManager {
+	private:
+		
+
+
+	public:
+
+};
+
+
+#endif // RENDER_TEXTUREMANAGER_HPP
